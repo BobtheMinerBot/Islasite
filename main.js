35932f623b155b5b8fc7899df6573b5cb2f3808f
unknown <bacallaojaasiel@gmail.com>
2023-09-28 19:23:46 -0700

    js update for sliders

--- main.js
+++ main.js
@@ -11,107 +11,76 @@ window.addEventListener('scroll', function() {
 
 
 
-    // Button click behavior for "Learn More" button
-    document.getElementById("learnmorebutton").addEventListener("click", function() {
-        fetch("/services")
-            .then(response => {
-                if (response.ok) {
-                    // If the response is successful, navigate to the "/services" page
-                    window.location.href = "services"; // Use a relative path
-                } else {
-                    console.error("Error fetching data from the server.");
-                }
-            })
-            .catch(error => {
-                console.error("Error: " + error);
-            });
-    });
-
-    // Button click behavior for "Contact Us" button
-    document.getElementById("ctabutton").addEventListener("click", function() {
-        fetch("/contact")
-            .then(response => {
-                if (response.ok) {
-                    // If the response is successful, navigate to the "/contact" page
-                    window.location.href = "contact"; // Use a relative path
-                } else {
-                    console.error("Error fetching data from the server.");
-                }
-            })
-            .catch(error => {
-                console.error("Error: " + error);
-            });
-    });
-    const testimonials = document.querySelectorAll('.testimonial');
-let currentIndex = 0;
-let userInteracted = false; // Track user interaction
-let intervalId; // Store the interval ID
-
-function showTestimonial(index) {
+  const testimonials = document.querySelectorAll('.testimonial');
+  let currentIndex = 0;
+  let userInteracted = false; // Track user interaction
+  let intervalId; // Store the interval ID
+  
+  function showTestimonial(index) {
     testimonials.forEach((testimonial, i) => {
-        if (i === index) {
-            testimonial.classList.add('active');
-        } else {
-            testimonial.classList.remove('active');
-        }
+      if (i === index) {
+        testimonial.classList.add('active');
+      } else {
+        testimonial.classList.remove('active');
+      }
     });
-}
-
-function nextTestimonial() {
+  }
+  
+  function nextTestimonial() {
     currentIndex = (currentIndex + 1) % testimonials.length;
     showTestimonial(currentIndex);
-}
-
-function prevTestimonial() {
+  }
+  
+  function prevTestimonial() {
     currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
     showTestimonial(currentIndex);
-}
-
-function startAutomaticCycling() {
+  }
+  
+  function startAutomaticCycling() {
     // Check if the user has interacted before cycling
     if (!userInteracted) {
-        nextTestimonial();
+      nextTestimonial();
     }
-}
-
-// Automatically cycle through testimonials every 5 seconds
-intervalId = setInterval(startAutomaticCycling, 5000);
-
-// Add event listeners to arrow buttons
-document.querySelector('.next-arrow').addEventListener('click', () => {
+  }
+  
+  // Automatically cycle through testimonials every 5 seconds
+  intervalId = setInterval(startAutomaticCycling, 5000);
+  
+  // Add event listeners to arrow buttons
+  document.querySelector('.next-arrow').addEventListener('click', () => {
     clearInterval(intervalId); // Stop the automatic cycling when user clicks next
     nextTestimonial();
     userInteracted = true; // Set user interaction flag
     setTimeout(() => {
-        userInteracted = false; // Reset user interaction flag after a delay
-        intervalId = setInterval(startAutomaticCycling, 5000); // Restart automatic cycling
+      userInteracted = false; // Reset user interaction flag after a delay
+      intervalId = setInterval(startAutomaticCycling, 5000); // Restart automatic cycling
     }, 1000); // Adjust the delay as needed
-});
-
-document.querySelector('.prev-arrow').addEventListener('click', () => {
+  });
+  
+  document.querySelector('.prev-arrow').addEventListener('click', () => {
     clearInterval(intervalId); // Stop the automatic cycling when user clicks previous
     prevTestimonial();
     userInteracted = true; // Set user interaction flag
     setTimeout(() => {
-        userInteracted = false; // Reset user interaction flag after a delay
-        intervalId = setInterval(startAutomaticCycling, 5000); // Restart automatic cycling
+      userInteracted = false; // Reset user interaction flag after a delay
+      intervalId = setInterval(startAutomaticCycling, 5000); // Restart automatic cycling
     }, 1000); // Adjust the delay as needed
-});
-
-// Show the first testimonial initially
-showTestimonial(currentIndex);
-
-// Disable automatic cycling while scrolling
-let scrollingTimer;
-window.addEventListener('scroll', () => {
+  });
+  
+  // Show the first testimonial initially
+  showTestimonial(currentIndex);
+  
+  // Disable automatic cycling while scrolling
+  let scrollingTimer;
+  window.addEventListener('scroll', () => {
     clearInterval(intervalId); // Stop automatic cycling on scroll
     clearTimeout(scrollingTimer); // Clear previous scrolling timer
     userInteracted = true; // Set user interaction flag on scroll
     scrollingTimer = setTimeout(() => {
-        userInteracted = false; // Reset user interaction flag after scrolling stops
-        intervalId = setInterval(startAutomaticCycling, 4000); // Restart automatic cycling
-    }, 1000); // Adjust the delay as needed
-});
-
-
-});
+      userInteracted = false; // Reset user interaction flag after scrolling stops
+      intervalId = setInterval(startAutomaticCycling, 4000); // Restart automatic cycling
+    }, 2000); // Adjust the delay as needed
+  });
+  
+  userInteracted = false; // Reset userInteracted flag immediately after scrolling
+  
